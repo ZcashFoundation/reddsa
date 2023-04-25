4f8ce48cd57b13c3b8765dca07136294df0c6e20
Marek <mail@marek.onl>
2023-04-25 19:51:13 +0200

    change: Refactor & optimize the NAF (#63)
    * Make the NAF function generic
    
    * Use the `jubjub` prefix for Jubjub types in tests
    
    * Add tests for the NAF for Jubjub & Pallas scalars
    
    * Use Rust's TryInto for [u8; 32]
    
    Co-authored-by: Conrado Gouvea <conradoplg@gmail.com>
    
    * Simplify the scalar conversion
    
    * Revert "Simplify the scalar conversion"
    
    This reverts commit f50ff9dd8ad4c79a561f68c28e6bac23790bbb32.
    
    * Revert "Use Rust's TryInto for [u8; 32]"
    
    This reverts commit 282c3b16ace5c53799b6706f451e079987e1100d.
    
    ---------
    
    Co-authored-by: Deirdre Connolly <deirdre@zfnd.org>
    Co-authored-by: Conrado Gouvea <conradoplg@gmail.com>

--- src/scalar_mul/tests.rs
+++ src/scalar_mul/tests.rs
@@ -1,35 +1,41 @@
 use alloc::vec::Vec;
-use group::GroupEncoding;
-use jubjub::{ExtendedPoint, Scalar};
+use group::{ff::Field, GroupEncoding};
+use num_bigint::BigInt;
+use num_traits::Zero;
+use rand::thread_rng;
 
 use crate::scalar_mul::VartimeMultiscalarMul;
+
+use super::NonAdjacentForm;
 
 /// Generates test vectors for [`test_jubjub_vartime_multiscalar_mul`].
 // #[test]
 #[allow(dead_code)]
 fn gen_jubjub_test_vectors() {
-    use group::{ff::Field, Group};
-    use rand::thread_rng;
+    use group::Group;
     use std::println;
 
     let rng = thread_rng();
 
-    let scalars = [Scalar::random(rng.clone()), Scalar::random(rng.clone())];
+    let scalars = [
+        jubjub::Scalar::random(rng.clone()),
+        jubjub::Scalar::random(rng.clone()),
+    ];
     println!("Scalars:");
     for scalar in scalars {
         println!("{:?}", scalar.to_bytes());
     }
 
     let points = [
-        ExtendedPoint::random(rng.clone()),
-        ExtendedPoint::random(rng),
+        jubjub::ExtendedPoint::random(rng.clone()),
+        jubjub::ExtendedPoint::random(rng),
     ];
     println!("Points:");
     for point in points {
         println!("{:?}", point.to_bytes());
     }
 
-    let res = ExtendedPoint::vartime_multiscalar_mul(scalars, points);
+    let res = jubjub::ExtendedPoint::vartime_multiscalar_mul(scalars, points);
     println!("Result:");
     println!("{:?}", res.to_bytes());
 }
@@ -65,21 +71,94 @@ fn test_jubjub_vartime_multiscalar_mul() {
         131, 180, 48, 148, 72, 212, 148, 212, 240, 77, 244, 91, 213,
     ];
 
-    let scalars: Vec<Scalar> = scalars
+    let scalars: Vec<jubjub::Scalar> = scalars
         .into_iter()
-        .map(|s| Scalar::from_bytes(&s).expect("Could not deserialize a `jubjub::Scalar`."))
+        .map(|s| jubjub::Scalar::from_bytes(&s).expect("Could not deserialize a `jubjub::Scalar`."))
         .collect();
 
-    let points: Vec<ExtendedPoint> = points
+    let points: Vec<jubjub::ExtendedPoint> = points
         .into_iter()
         .map(|p| {
-            ExtendedPoint::from_bytes(&p).expect("Could not deserialize a `jubjub::ExtendedPoint`.")
+            jubjub::ExtendedPoint::from_bytes(&p)
+                .expect("Could not deserialize a `jubjub::ExtendedPoint`.")
         })
         .collect();
 
-    let expected_product = ExtendedPoint::from_bytes(&expected_product)
+    let expected_product = jubjub::ExtendedPoint::from_bytes(&expected_product)
         .expect("Could not deserialize a `jubjub::ExtendedPoint`.");
 
-    let product = ExtendedPoint::vartime_multiscalar_mul(scalars, points);
+    let product = jubjub::ExtendedPoint::vartime_multiscalar_mul(scalars, points);
     assert_eq!(expected_product, product);
+}
+
+/// Tests the non-adjacent form for a Jubjub scalar.
+#[test]
+fn test_non_adjacent_form() {
+    let rng = thread_rng();
+
+    let scalar = jubjub::Scalar::random(rng);
+    test_non_adjacent_form_for_scalar(5, scalar);
+}
+
+/// Tests the non-adjacent form for a particular scalar.
+pub(crate) fn test_non_adjacent_form_for_scalar<Scalar: NonAdjacentForm>(w: usize, scalar: Scalar) {
+    let naf = scalar.non_adjacent_form(w);
+    let naf_length = Scalar::naf_length();
+
+    // Check that the computed w-NAF has the intended length.
+    assert_eq!(naf.len(), naf_length);
+
+    let w = u32::try_from(w).expect("The window `w` did not fit into `u32`.");
+
+    // `bound` <- 2^(w-1)
+    let bound = 2_i32.pow(w - 1);
+
+    // `valid_coeffs` <- a range of odd integers from -2^(w-1) to 2^(w-1)
+    let valid_coeffs: Vec<i32> = (-bound..bound).filter(|x| x.rem_euclid(2) == 1).collect();
+
+    let mut reconstructed_scalar: BigInt = Zero::zero();
+
+    // Reconstruct the original scalar, and check two general invariants for any w-NAF along the
+    // way.
+    let mut i = 0;
+    while i < naf_length {
+        if naf[i] != 0 {
+            // In a w-NAF, every nonzero coefficient `naf[i]` is an odd signed integer with
+            // -2^(w-1) < `naf[i]` < 2^(w-1).
+            assert!(valid_coeffs.contains(&i32::from(naf[i])));
+
+            // Incrementally keep reconstructing the original scalar.
+            reconstructed_scalar += naf[i] * BigInt::from(2).pow(i.try_into().unwrap());
+
+            // In a w-NAF, at most one of any `w` consecutive coefficients is nonzero.
+            for _ in 1..w {
+                i += 1;
+                if i >= naf_length {
+                    break;
+                }
+                assert_eq!(naf[i], 0)
+            }
+        }
+
+        i += 1;
+    }
+
+    // Check that the reconstructed scalar is not negative, and convert it to little-endian bytes.
+    let reconstructed_scalar = reconstructed_scalar
+        .to_biguint()
+        .expect("The reconstructed scalar is negative.")
+        .to_bytes_le();
+
+    // Check that the reconstructed scalar is not too big.
+    assert!(reconstructed_scalar.len() <= 32);
+
+    // Convert the reconstructed scalar to a fixed byte array so we can compare it with the orginal
+    // scalar.
+    let mut reconstructed_scalar_bytes: [u8; 32] = [0; 32];
+    for (i, byte) in reconstructed_scalar.iter().enumerate() {
+        reconstructed_scalar_bytes[i] = *byte;
+    }
+
+    // Check that the reconstructed scalar matches the original one.
+    assert_eq!(reconstructed_scalar_bytes, scalar.inner_to_bytes());
 }
