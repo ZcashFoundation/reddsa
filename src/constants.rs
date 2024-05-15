5a0bde12245ff44fb2efb37ef414605560d642ae
Marco Granelli <marco.granelli@ymail.com>
2024-05-15 10:52:54 +0200

    Updates the basepoints based on the masp trusted setup

--- src/constants.rs
+++ src/constants.rs
@@ -8,17 +8,17 @@
 // - Henry de Valence <hdevalence@hdevalence.ca>
 
 /// The byte-encoding of the basepoint for `SpendAuthSig`.
-// Extracted ad-hoc from librustzcash
+// Extracted ad-hoc from the MASP trusted setup
 // XXX add tests for this value.
 pub const SPENDAUTHSIG_BASEPOINT_BYTES: [u8; 32] = [
-    48, 181, 242, 170, 173, 50, 86, 48, 188, 221, 219, 206, 77, 103, 101, 109, 5, 253, 28, 194,
-    208, 55, 187, 83, 117, 182, 233, 109, 158, 1, 161, 215,
+    177, 180, 134, 161, 35, 38, 182, 20, 82, 253, 36, 246, 49, 208, 18, 32, 242, 158, 244, 241,
+    207, 254, 222, 117, 171, 224, 82, 30, 159, 95, 188, 12,
 ];
 
 /// The byte-encoding of the basepoint for `BindingSig`.
-// Extracted ad-hoc from librustzcash
+// Extracted ad-hoc from the MASP trusted setup
 // XXX add tests for this value.
 pub const BINDINGSIG_BASEPOINT_BYTES: [u8; 32] = [
-    139, 106, 11, 56, 185, 250, 174, 60, 59, 128, 59, 71, 176, 241, 70, 173, 80, 171, 34, 30, 110,
-    42, 251, 230, 219, 222, 69, 203, 169, 211, 129, 237,
+    208, 146, 230, 156, 233, 252, 229, 40, 254, 2, 3, 54, 170, 45, 76, 249, 80, 17, 174, 184, 212,
+    12, 144, 188, 11, 213, 32, 183, 249, 17, 95, 85,
 ];
